5d1eb3b27949cb3ce2b1e4618648be88584cb32e
Winston Pacheco Junior <winston.pacheco@gmail.com>
2024-11-21 22:26:32 -0300

    removing unecessary comments

--- internal/entity/result.go
+++ internal/entity/result.go
@@ -5,14 +5,6 @@ import (
 	"time"
 )
 
-/*
-Apresentar um relatório ao final dos testes contendo:
-Tempo total gasto na execução
-Quantidade total de requests realizados.
-Quantidade de requests com status HTTP 200.
-Distribuição de outros códigos de status HTTP (como 404, 500, etc.).
-*/
-
 type Results struct {
 	TotalTime     time.Duration
 	TotalRequests uint64
